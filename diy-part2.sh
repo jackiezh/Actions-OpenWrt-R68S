0e4d7289e71ec1bf525d5797706d54db4ea185c5
钟亮明 <zhongliangming@ascs.tech>
2023-05-09 14:24:38 +0800

    修改默认ip和密码

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP & password
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i '1c root:$1$w4fjHeEK$PHl6CC2NESlaa7C7AP9PU.:19325:0:99999:7:::' package/base-files/files/etc/shadow

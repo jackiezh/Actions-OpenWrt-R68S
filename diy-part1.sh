edd6adc023236eb656e9c39ecc8be4e37f4f657f
钟亮明 <zhongliangming@ascs.tech>
2023-05-18 21:15:40 +0800

    切换6.1内核并冲定义网卡接口

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,15 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 切换6.1内核
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
+
+# 重新定义网卡
+rm -f target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+cp $GITHUB_WORKSPACE/02_network target/linux/rockchip/armv8/base-files/etc/board.d
+
+# 替换默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
